19907c3ac102a9557d5ecb2f04c355010e929115
Ilya Zakharov <soapmangoesdown@gmail.com>
2018-01-17 14:34:16 +0300

    Update quote.py

--- quote.py
+++ quote.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
 
+import requests
+
+r = requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=text")
+quote = r.text.replace('  ', ' ').replace(' )', ')').replace('  ', ' ').strip()
+print(quote)
