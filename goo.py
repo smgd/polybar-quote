1a47a8fb1154e2dfaab43545ff8eecfa58d9287a
Ilya Zakharov <soapmangoesdown@gmail.com>
2018-01-18 06:34:15 +0300

    Create goo.py

---
+++ goo.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+me = subprocess.check_output("whoami", shell=True).decode("utf-8").strip()
+
+with open('/home/' + me + '/.config/polybar/random-quote/log.txt') as qLog:
+    command = "xdg-open 'https://www.google.ru/search?q=" + qLog.readline() + "'"
+os.system(command)
